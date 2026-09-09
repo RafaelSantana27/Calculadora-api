a0681d9073b9450eeca106b3365e57b750e5d793
Rafael Silva <santana_r@outlook.com>
2026-09-08 22:24:42 -0300

    Construindo o Service

---
+++ Calculadora-api/Services/CalculadoraService.cs
@@ -0,0 +1,28 @@
+﻿namespace Calculadora_api.Services
+{
+    public class CalculadoraService
+    {
+        public decimal Somar(decimal primeiroNumero, decimal segundoNumero)
+        {
+            return primeiroNumero + segundoNumero;
+        }
+        public decimal Subtrair(decimal primeiroNumero, decimal segundoNumero)
+        {
+            return primeiroNumero - segundoNumero;
+        }
+        public decimal Multiplicar(decimal primeiroNumero, decimal segundoNumero)
+        {
+            return primeiroNumero * segundoNumero;
+        }
+        public decimal Dividir(decimal primeiroNumero, decimal segundoNumero)
+        {
+            if (segundoNumero == 0)
+                throw new DivideByZeroException("Divisão por zero não é permitida.");
+            return primeiroNumero / segundoNumero;
+        }
+        public decimal Media(decimal primeiroNumero, decimal segundoNumero)
+        {
+            return (primeiroNumero + segundoNumero) / 2;
+        }
+    }
+}
