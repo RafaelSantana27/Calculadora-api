248234a9fcb831b77548e9ea3381b9e4ebf7b94f
Rafael Silva <santana_r@outlook.com>
2026-09-08 22:46:41 -0300

    Construindo validador dos numeros

---
+++ Calculadora-api/Utils/HelperNumero.cs
@@ -0,0 +1,21 @@
+﻿namespace Calculadora_api.Utils
+{
+    public class HelperNumero
+    {
+        public static bool EhNumero(string strNumero)
+        {
+            decimal numero;
+
+            return decimal.TryParse(strNumero, out numero);
+        }
+
+        public static decimal ConverterParaDecimal(string strNumero)
+        {
+            decimal numero;
+
+            if (decimal.TryParse(strNumero, out numero))
+                return numero;
+            return 0;
+        }
+    }
+}
