0627e003bd112478e8bded49dc4192e5af927d9f
Rafael Silva <santana_r@outlook.com>
2026-09-09 21:45:39 -0300

    Configurado injeção de dependência do HistoricoService como Singleton

--- Calculadora-api/Program.cs
+++ Calculadora-api/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddSingleton<CalculadoraService>();
+builder.Services.AddSingleton<HistoricoService>();
 
 var app = builder.Build();
 
