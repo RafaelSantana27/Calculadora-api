ba908ff331917a307b00b8b3406c1631a177fea8
Rafael Silva <santana_r@outlook.com>
2026-09-09 21:46:55 -0300

    Criado Model de Historico

---
+++ Calculadora-api/Model/Historico.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculadora_api.Model
+{
+    public class Historico
+    {
+        public string Operacao { get; set; }
+        public decimal PrimeiroNumero { get; set; }
+        public decimal SegundoNumero { get; set; }
+        public decimal Resultado { get; set; }
+        public DateTime DataHoraOperacao { get; set; }
+
+        public Historico(string operacao, decimal primeiroNumero, decimal segundoNumero, decimal resultado, DateTime dataHoraOperacao)
+        {
+            Operacao = operacao;
+            PrimeiroNumero = primeiroNumero;
+            SegundoNumero = segundoNumero;
+            Resultado = resultado;
+            DataHoraOperacao = dataHoraOperacao;
+        }
+    }
+}
