2f5ced75a9d97f651b8eecef28ac8a9448235a30
Rafael Silva <santana_r@outlook.com>
2026-09-09 21:50:51 -0300

    Refatorado todo o Service para aplicar o novo serviço de Historico apos realizar a operação

--- Calculadora-api/Services/CalculadoraService.cs
+++ Calculadora-api/Services/CalculadoraService.cs
@@ -2,27 +2,48 @@
 {
     public class CalculadoraService
     {
+        private readonly HistoricoService _historicoService;
+
+        public CalculadoraService(HistoricoService historicoService)
+        {
+            _historicoService = historicoService;
+        }
+
         public decimal Somar(decimal primeiroNumero, decimal segundoNumero)
         {
-            return primeiroNumero + segundoNumero;
+            var resultado = primeiroNumero + segundoNumero;
+
+            //_historicoService.AdicionarHistorico($"Soma: {primeiroNumero} + {segundoNumero} = {resultado}");
+            _historicoService.AdicionarHistorico($"Soma,{primeiroNumero},{segundoNumero},{resultado}");
+
+            return resultado;
         }
         public decimal Subtrair(decimal primeiroNumero, decimal segundoNumero)
         {
-            return primeiroNumero - segundoNumero;
+            var resultado = primeiroNumero - segundoNumero;
+            _historicoService.AdicionarHistorico($"Subtração,{primeiroNumero},{segundoNumero},{resultado}");
+            return resultado;
         }
         public decimal Multiplicar(decimal primeiroNumero, decimal segundoNumero)
         {
-            return primeiroNumero * segundoNumero;
+            var resultado = primeiroNumero * segundoNumero;
+            _historicoService.AdicionarHistorico($"Multiplicação,{primeiroNumero},{segundoNumero},{resultado}");
+            return resultado;
         }
         public decimal Dividir(decimal primeiroNumero, decimal segundoNumero)
         {
             if (segundoNumero == 0)
                 throw new DivideByZeroException("Divisão por zero não é permitida.");
-            return primeiroNumero / segundoNumero;
+
+            var resultado = primeiroNumero / segundoNumero;
+            _historicoService.AdicionarHistorico($"Divisão,{primeiroNumero},{segundoNumero},{resultado}");
+            return resultado;
         }
         public decimal Media(decimal primeiroNumero, decimal segundoNumero)
         {
-            return (primeiroNumero + segundoNumero) / 2;
+            var resultado = (primeiroNumero + segundoNumero) / 2;
+            _historicoService.AdicionarHistorico($"Média,{primeiroNumero},{segundoNumero},{resultado}");
+            return resultado;
         }
     }
 }
---
+++ Calculadora-api/Services/HistoricoService.cs
@@ -0,0 +1,65 @@
+﻿using Calculadora_api.Model;
+using System.Text.Json;
+
+namespace Calculadora_api.Services
+{
+    public class HistoricoService
+    {
+        private readonly List<Historico> _historico;
+
+        public HistoricoService()
+        {
+            _historico = new List<Historico>();
+        }
+        public void AdicionarHistorico(string operacao)
+        {
+            string[] partes = operacao.Split(',');
+
+            _historico.Add(new Historico(
+                    partes[0],
+                    decimal.Parse(partes[1]),
+                    decimal.Parse(partes[2]),
+                    decimal.Parse(partes[3]),
+                    DateTime.Now)
+                );
+
+            Console.WriteLine($"Operação adicionada ao histórico: {operacao}");
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+
+            //string fileName = "$./Repository/HistoricoCalculadora.json";
+            string fileName = $"./Repository/HistoricoCalculadora_{DateTime.Now:yyyy-MM-dd}.json";
+
+            using FileStream createStram = File.Create(fileName);
+            JsonSerializer.SerializeAsync(createStram, _historico, options);
+
+            //string jsonString = JsonSerializer.Serialize(_historico, options);
+
+            //Console.WriteLine(jsonString);
+
+
+            //_historico.Add(operacao);
+            //Console.WriteLine($"Operação adicionada ao histórico: {operacao}");
+
+            //string fileName = "./Repository/HistoricoCalculadora.json";
+            //await using FileStream createStram = File.Create(fileName);
+            //await JsonSerializer.SerializeAsync(createStram, _historico);
+            //Console.WriteLine(File.ReadAllText(fileName));
+
+        }
+        public List<Historico> ObterHistorico()
+        {
+            string fileName = $"./Repository/HistoricoCalculadora_{DateTime.Now:yyyy-MM-dd}.json";
+
+            if (!File.Exists(fileName))
+            {
+                return new List<Historico>();
+            }
+
+            string jsonString = File.ReadAllText(fileName);
+
+            return JsonSerializer.Deserialize<List<Historico>>(jsonString) ?? new List<Historico>();
+        }
+    }
+}
+
